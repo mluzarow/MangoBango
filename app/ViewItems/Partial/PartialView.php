37ef63b0f303b4d1000cf95203de6a66bab01051
Mackenzie <ma.luzarow@gmail.com>
2018-06-13 22:17:05 -0500

    Changed to php.

---
+++ app/ViewItems/Partial/PartialView.php
@@ -0,0 +1,29 @@
+<?php
+namespace ViewItems\Partial;
+
+abstract class PartialView {
+	protected $error_templates = [
+		'InvalidArgumentException' => '%s::%s Argument (%s) must be %s; %s given.'
+	];
+	protected $output;
+	
+	abstract protected function constructView ();
+	abstract protected function processParameters (array $view_params);
+	
+	protected final function throwException (Throwable $exception, array $details = null) : null {
+		if (!empty($details)) {
+			new $$exception (
+				vsprintf (
+					$error_templates[$exception],
+					$details
+				)
+			);
+		} else {
+			new $$exception ();
+		}
+	}
+	
+	public final function render () {
+		return ($this->output);
+	}
+}

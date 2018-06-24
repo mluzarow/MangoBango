0c8fbcfe6959762b23315175227588a63bc06b89
Mackenzie <ma.luzarow@gmail.com>
2018-06-23 21:40:51 -0500

    Added necessary abstract decs in view.

--- app/ViewItems/PageViews/ConfigView.php
+++ app/ViewItems/PageViews/ConfigView.php
@@ -4,5 +4,24 @@ namespace ViewItems\PageViews;
 use \ViewItems\ViewAbstract;
 
 class ConfigView extends ViewAbstract {
+	/**
+	 * Constructs the CSS using the available properties.
+	 */
+	protected function constructCSS () {
+		return ('');
+	}
 	
+	/**
+	 * Constructs the HTML using the available properties.
+	 */
+	protected function constructHTML () {
+		return ('');
+	}
+	
+	/**
+	 * Constructs the javascript using the available properties.
+	 */
+	protected function constructJavascript () {
+		return ('');
+	}
 }

d9ce99a017d238683455069f2d6ad42d38e8e30a
Mackenzie <ma.luzarow@gmail.com>
2018-06-23 22:05:47 -0500

    Added controller & view data setup.

--- app/Controllers/Config.php
+++ app/Controllers/Config.php
@@ -1,6 +1,8 @@
 <?php
 namespace Controllers;
 
+use ViewItems\PageViews\ConfigView;
+
 /**
  * Controller for processing configs to display on the configs page.
  */
@@ -9,6 +11,20 @@ class Config {
 	 * Constructor for page controller Config.
 	 */
 	public function __construct () {
+		$q = '
+			SELECT `config_name`, `config_value`
+			FROM `server_configs`';
+		$r = \Core\Database::query ($q);
 		
+		$configs_dict = [];
+		foreach ($r as $row) {
+			$configs_dict[$row['config_name']] = $row['config_value'];
+		}
+		
+		$view_parameters = [];
+		$view_parameters['reader_display_style'] = $configs_dict['reader_display_style'];
+		
+		$view = new ConfigView ($view_parameters);
+		echo $view->render ();
 	}
 }
--- app/ViewItems/PageViews/ConfigView.php
+++ app/ViewItems/PageViews/ConfigView.php
@@ -27,4 +27,28 @@ class ConfigView extends ViewAbstract {
 	protected function constructJavascript () {
 		return ('');
 	}
+	
+	/**
+	 * Gets the reader display style setting.
+	 * 
+	 * @return int reader display style setting
+	 */
+	protected function getReaderDisplayStyle () {
+		return ($this->reader_display_style);
+	}
+	
+	/**
+	 * Sets the reader display style setting.
+	 * 
+	 * @param int $config reader display style setting
+	 * 
+	 * @throws InvalidArgumentException on config value out of bounds
+	 */
+	protected function setReaderDisplayStyle (int $config) {
+		if (!in_array($config, [1, 2])) {
+			throw new \InvalidArgumentException ('Argument (Reader Display Style) value must in set [1, 2]; '.$config.' given.');
+		}
+		
+		$this->reader_display_style = $config;
+	}
 }

707bb06603a87366a4369f95a90ae727228d2b4c
Mackenzie <ma.luzarow@gmail.com>
2018-06-23 23:40:33 -0500

    Updated config page view.

--- app/ViewItems/PageViews/ConfigView.php
+++ app/ViewItems/PageViews/ConfigView.php
@@ -11,21 +11,113 @@ class ConfigView extends ViewAbstract {
 	 * Constructs the CSS using the available properties.
 	 */
 	protected function constructCSS () {
-		return ('');
+		$output =
+		'<style>
+			.config_list_wrap .title {
+				text-align: center;
+				font-family: Arial;
+				font-size: 2em;
+			}
+			
+			.config_list_wrap .config_list {
+				padding: 30px;
+			}
+			
+			.config_list_wrap .config_list .config_wrap {
+				padding-bottom: 20px;
+				font-size: 1.5em;
+			}
+			
+			.config_list_wrap .config_list .config_wrap select,
+			.config_list_wrap .config_list .config_wrap input {
+				margin-left: 15px;
+				font-size: 0.9em;
+			}
+			
+			.config_list_wrap .config_list .config_wrap input {
+				width: 550px;
+			}
+			
+			.config_list_wrap .submit_btn {
+				margin-left: 20px;
+				padding: 10px 30px;
+				display: inline-block;
+				background-color: #d68100;
+				cursor: pointer;
+				font-family: Arial;
+				font-size: 1.5em;
+			}
+			
+			.config_list_wrap .submit_btn:hover {
+				background-color: #ffaf00;
+			}
+		</style>';
+		
+		return ($output);
 	}
 	
 	/**
 	 * Constructs the HTML using the available properties.
 	 */
 	protected function constructHTML () {
-		return ('');
+		$output =
+		'<div class="config_list_wrap">
+			<h2 class="title">Server Settings</h2>
+			<form class="config_list">
+				<div class="config_wrap">
+					<label>Reader Display Style</label>
+					<select id="reader_display_style" autocomplete="off">
+						<option value="1" '.($this->getReaderDisplayStyle () === 1 ? 'selected' : '').'>Display as single panel</option>
+						<option value="2" '.($this->getReaderDisplayStyle () === 2 ? 'selected' : '').'>Display as continous strip</option>
+					</select>
+				</div>
+				<div class="config_wrap">
+					<label>Manga directory</label>
+					<input id="manga_directory" value="'.$this->getMangaDirectory ().'" type="text" autocomplete="off" />
+				</div>
+			</form>
+			<div class="submit_btn">Save</div>
+		</div>';
+		
+		return ($output);
 	}
 	
 	/**
 	 * Constructs the javascript using the available properties.
 	 */
 	protected function constructJavascript () {
-		return ('');
+		$output =
+		'<script>
+			$(window).ready (function () {
+				$(".config_list_wrap .submit_btn").click (function () {
+					var configs = {};
+					
+					configs["reader_display_style"] = $("#reader_display_style").val ();
+					configs["manga_directory"] = $("#manga_directory").val ();
+					
+					$.ajax ({
+						url: "ajax/Controllers/Config/ajaxUpdateConfigs",
+						method: "POST",
+						data: {
+							configs: JSON.stringify (configs)
+						}
+					}).done (function () {
+						location.reload ();
+					});
+				});
+			});
+		</script>';
+		
+		return ($output);
+	}
+	
+	/**
+	 * Gets the manga directory setting.
+	 * 
+	 * @return string manga directory setting
+	 */
+	protected function getMangaDirectory () {
+		return ($this->manga_directory);
 	}
 	
 	/**
@@ -38,10 +130,29 @@ class ConfigView extends ViewAbstract {
 	}
 	
 	/**
+	 * Sets the manga directory setting.
+	 * 
+	 * @param string $config manga directory setting
+	 *
+	 * @throws TypeError on non-string config value
+	 * @throws InvalidArgumentException on empty config value
+	 */
+	protected function setMangaDirectory (string $config) {
+		$config = trim ($config);
+		
+		if (empty ($config)) {
+			throw new InvalidArgumentException ('Argument (Manga Directory) can not be empty.');
+		}
+		
+		$this->manga_directory = $config;
+	}
+	
+	/**
 	 * Sets the reader display style setting.
 	 * 
 	 * @param int $config reader display style setting
 	 * 
+	 * @throws TypeError on non-integer config value
 	 * @throws InvalidArgumentException on config value out of bounds
 	 */
 	protected function setReaderDisplayStyle (int $config) {

93b2a5c8ff8f3657c5726bd54243457740687f87
Mackenzie <ma.luzarow@gmail.com>
2018-06-23 21:42:48 -0500

    Added comments to page controller.

--- app/Controllers/Config.php
+++ app/Controllers/Config.php
@@ -1,9 +1,14 @@
 <?php
 namespace Controllers;
 
+/**
+ * Controller for processing configs to display on the configs page.
+ */
 class Config {
+	/**
+	 * Constructor for page controller Config.
+	 */
 	public function __construct () {
-		$output =
-		''
+		
 	}
 }

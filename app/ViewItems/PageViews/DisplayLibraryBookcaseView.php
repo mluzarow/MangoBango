242fd1391a5fb94c200b2a0af9cf2787288bd60f
Mackenzie <ma.luzarow@gmail.com>
2018-06-24 20:41:14 -0500

    Added bookcase view class skeleton.

---
+++ app/ViewItems/PageViews/DisplayLibraryBookcaseView.php
@@ -0,0 +1,27 @@
+<?php
+namespace ViewItems\PageViews;
+
+use ViewItems\ViewAbstract;
+
+class DisplayLibraryBookcaseView extends ViewAbstract {
+	/**
+	 * Constructs the CSS using the available properties.
+	 */
+	protected function constructCSS () {
+		return ('');
+	}
+	
+	/**
+	 * Constructs the HTML using the available properties.
+	 */
+	protected function constructHTML () {
+		return ('');
+	}
+	
+	/**
+	 * Constructs the javascript using the available properties.
+	 */
+	protected function constructJavascript () {
+		return ('');
+	}
+}

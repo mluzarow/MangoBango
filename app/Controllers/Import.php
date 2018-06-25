0d22e561ffc2ced382d89aa93b662e94f0df4fee
Mackenzie <ma.luzarow@gmail.com>
2018-06-25 18:55:41 -0500

    Added page controller file for import page.

---
+++ app/Controllers/Import.php
@@ -0,0 +1,15 @@
+<?php
+namespace Controllers;
+
+/**
+ * Page controller managing manga importing and guiding user through the process
+ * of cataloging files and metadata tagging.
+ */
+class Import {
+	/**
+	 * Constructor for page controller Import.
+	 */
+	public function __construct () {
+		echo get_class($this);
+	}
+}

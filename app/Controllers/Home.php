e60c4a76c6b793da0f5ad3656f9ce88897dee778
Mackenzie <ma.luzarow@gmail.com>
2018-06-30 13:51:17 -0500

    Updated home page to show real statistics.

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -3,7 +3,42 @@ namespace Controllers;
 
 class Home {
 	public function __construct () {
-		$view = new \ViewItems\PageViews\HomeView ([]);
+		$view_parameters = [];
+		$view_parameters['box_contents'] = [];
+		// Get number of series
+		$q = '
+			SELECT COUNT(*) AS `count`
+			FROM `manga_directories_series`';
+		$r = \Core\Database::query ($q);
+		
+		$view_parameters['box_contents'][] = [
+			'title' => 'Number of series',
+			'value' => $r[0]['count']
+		];
+		
+		// Get number of volumes
+		$q = '
+			SELECT COUNT(*) AS `count`
+			FROM `manga_directories_volumes`';
+		$r = \Core\Database::query ($q);
+		
+		$view_parameters['box_contents'][] = [
+			'title' => 'Number of volumes',
+			'value' => $r[0]['count']
+		];
+		
+		// Get number of chapters
+		$q = '
+			SELECT COUNT(*) AS `count`
+			FROM `manga_directories_chapters`';
+		$r = \Core\Database::query ($q);
+		
+		$view_parameters['box_contents'][] = [
+			'title' => 'Number of chapters',
+			'value' => $r[0]['count']
+		];
+		
+		$view = new \ViewItems\PageViews\HomeView ($view_parameters);
 		echo $view->render ();
 	}
 }
--- app/ViewItems/PageViews/HomeView.php
+++ app/ViewItems/PageViews/HomeView.php
@@ -69,38 +69,18 @@ class HomeView extends ViewAbstract {
 	protected function constructHTML () {
 		$output =
 		'<h2 class="home_header">Library Statistics</h2>
-		<div class="statbox_grid_display">
-			<div class="statbox_wrap">
-				<div class="title">Number of series</div>
-				<div class="statbox_inner_wrap">
-					<span>6666</span>
-				</div>
-			</div>
-			<div class="statbox_wrap">
-				<div class="title">Number of series</div>
-				<div class="statbox_inner_wrap">
-					<span>6666</span>
-				</div>
-			</div>
-			<div class="statbox_wrap">
-				<div class="title">Number of series</div>
-				<div class="statbox_inner_wrap">
-					<span>6666</span>
-				</div>
-			</div>
-			<div class="statbox_wrap">
-				<div class="title">Number of series</div>
-				<div class="statbox_inner_wrap">
-					<span>6666</span>
-				</div>
-			</div>
-			<div class="statbox_wrap">
-				<div class="title">Number of series</div>
-				<div class="statbox_inner_wrap">
-					<span>6666</span>
-				</div>
-			</div>
-		</div>';
+		<div class="statbox_grid_display">';
+			foreach ($this->getBoxContents() as $box) {
+				$output .=
+				'<div class="statbox_wrap">
+					<div class="title">'.$box['title'].'</div>
+					<div class="statbox_inner_wrap">
+						<span>'.$box['value'].'</span>
+					</div>
+				</div>';
+			}
+		$output .=
+		'</div>';
 		
 		return ($output);
 	}
@@ -110,5 +90,39 @@ class HomeView extends ViewAbstract {
 	 */
 	protected function constructJavascript () {
 		return ('');
+	}
+	
+	/**
+	 * Gets the box contents list.
+	 * 
+	 * @return array box contents list
+	 */
+	protected function getBoxContents () {
+		return ($this->box_contents);
+	}
+	
+	/**
+	 * Set the box contents list.
+	 * 
+	 * @param array $box_contents box contents list
+	 * 
+	 * @throws TypeError on non-array parameter
+	 * @throws InvalidArgumentException on non-array items or missing item keys
+	 */
+	protected function setBoxContents (array $box_contents) {
+		foreach ($box_contents as $box) {
+			if (!is_array($box)) {
+				throw new \InvalidArgumentException('Argument (Box Contents) items must be arrays; '.gettype ($box).' given.');
+			}
+			
+			if (!array_key_exists ('title', $box)) {
+				throw new \InvalidArgumentException('Argument (Box Contents) items must have key "title".');
+			}
+			if (!array_key_exists ('value', $box)) {
+				throw new \InvalidArgumentException('Argument (Box Contents) items must have key "value".');
+			}
+		}
+		
+		$this->box_contents = $box_contents;
 	}
 }

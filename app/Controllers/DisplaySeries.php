66c4484f80aefcf06a673ae46539251b28c84ed2
Mackenzie <ma.luzarow@gmail.com>
2018-06-30 11:00:20 -0500

    Updated volume view to get data from db.

--- app/Controllers/DisplaySeries.php
+++ app/Controllers/DisplaySeries.php
@@ -11,71 +11,53 @@ class DisplaySeries {
 	 * Constructor for controller DisplaySeries.
 	 */
 	public function __construct () {
+		// Fetch manga directory
 		$q = '
 			SELECT `config_value` FROM `server_configs`
 			WHERE `config_name` = "manga_directory"';
 		$r = \Core\Database::query ($q);
 		
-		$test_directory = $r[0]['config_value'].'\\'.$_GET['series'];
-		$directory_tree = $this->dirToArray ($test_directory);
+		$manga_directory = $r[0]['config_value'];
+		
+		// Fetch manga info by ID
+		$q = '
+			SELECT `s`.`path`, `v`.*
+			FROM `manga_directories_series` AS `s`
+			JOIN `manga_directories_volumes` AS `v`
+				ON `s`.`manga_id` = `v`.`manga_id`
+			WHERE `s`.`manga_id` = '.$_GET['series'];
+		$r = \Core\Database::query ($q);
+		
+		if ($r === false) {
+			return;
+		}
 		
 		$view_parameters = [];
 		$view_parameters['volumes'] = [];
-		foreach ($directory_tree as $volume_folder => $volume_contents) {
-			// Skip loose files like images in the series folder
-			if (!is_array($volume_contents)) {
-				continue;
+		
+		foreach ($r as $v) {
+			if (empty($v['cover'])) {
+				// @TODO Should probably include a placeholder cover image.
+				throw new Exception('NO COVER FOR '.$v['path']);
 			}
 			
-			foreach ($volume_contents as $chapter) {
-				// Skip folders
-				if (is_array ($chapter)) {
-					continue;
-				}
-				
-				if (!empty (preg_match ('/^cover\.(?:png|jpg)$/', $chapter))) {
-					$file_path = "{$test_directory}\\{$volume_folder}\\{$chapter}";
-					
-					$f = fopen ($file_path, 'r');
-					$blob = fread ($f, filesize ($file_path));
-					fclose ($f);
-					
-					$ext = explode ('.', $chapter)[1];
-					
-					$view_parameters['volumes'][] = [
-						'link' => "/reader?series={$_GET['series']}&volume={$volume_folder}&chapter=c0001",
-						'source' => "data:image/{$ext};base64,".base64_encode ($blob)
-					];
-				}
-			}
+			$file_path = "{$manga_directory}\\{$v['path']}\\{$v['filename']}\\cover.{$v['cover']}";
+			
+			$f = fopen ($file_path, 'r');
+			$blob = fread ($f, filesize ($file_path));
+			fclose ($f);
+			
+			$file_segs = explode ('.', $v['path']);
+			$ext = end ($file_segs);
+			
+			$view_parameters['volumes'][] = [
+				'link' => "/reader?series={$v['manga_id']}&volume={$v['sort']}&chapter=1",
+				'source' => "data:image/{$ext};base64,".base64_encode ($blob)
+			];
 		}
 		
 		$view = new DisplaySeriesView ($view_parameters);
 		
 		echo $view->render ();
-	}
-	
-	/**
-	 * Create an array reflecting the directory structure at a given location.
-	 *  
-	 * @param string $dir file path to folder
-	 * 
-	 * @return array directory structure
-	 */
-	private function dirToArray ($dir) {
-		$result = array();
-		
-		$cdir = scandir($dir);
-		foreach ($cdir as $key => $value) {
-			if (!in_array ($value, array ('.', '..'))) {
-				if (is_dir ($dir . DIRECTORY_SEPARATOR . $value)) {
-					$result[$value] = $this->dirToArray ($dir . DIRECTORY_SEPARATOR . $value);
-				} else {
-					$result[] = $value;
-				}
-			}
-		}
-		
-		return ($result);
 	}
 }

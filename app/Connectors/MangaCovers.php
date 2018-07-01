8225fc56c77b19228821042c2538315109c4b9a6
Mackenzie <ma.luzarow@gmail.com>
2018-07-01 09:17:40 -0500

    Added methods for sending post and get requests.

--- app/Connectors/MangaCovers.php
+++ app/Connectors/MangaCovers.php
@@ -1,6 +1,58 @@
 <?php
 namespace Connectors;
 
+/**
+ * Connector for the manga covers database API. Used to draw cover, spine, index,
+ * and other volume images for each manga.
+ * 
+ * @link https://mcd.iosphe.re/#api MCD API Documentation
+ */
 class MangaCovers {
+	/**
+	 * @var string MCD API base URL
+	 */
+	private $base_url = 'https://mcd.iosphe.re/api/v1/';
 	
+	/**
+	 * Performs a cURL GET request with the constructed URL.
+	 * 
+	 * @param string $url constructed URL to be used
+	 * 
+	 * @return string JSON response from the MCD servers
+	 */
+	private function requestGET ($url) {
+		$curl = curl_init ();
+		
+		curl_setopt ($curl, CURLOPT_URL, $url);
+		curl_setopt ($curl, CURLOPT_RETURNTRANSFER, true);
+		
+		$response = curl_exec ($curl);
+		
+		curl_close ($curl);
+		
+		return ($response);
+	}
+	
+	/**
+	 * Performs a cURL POST request with the constructed URL & included POST
+	 * variables.
+	 * 
+	 * @param string $url       constructed URL to be used
+	 * @param array  $post_vars variables to be sent via POST
+	 * 
+	 * @return string JSON response from the MCD servers
+	 */
+	private function requestPOST ($url, $post_vars) {
+		$curl = curl_init ();
+		
+		curl_setopt ($curl, CURLOPT_URL, $url);
+		curl_setopt ($curl, CURLOPT_POST, count ($post_vars));
+		curl_setopt ($curl, CURLOPT_POSTFIELDS, $post_vars);
+		
+		$response = curl_exec ($curl);
+		
+		curl_close ($curl);
+		
+		return ($response);
+	}
 }

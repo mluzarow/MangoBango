a9908a6baa800d16e964c17e1785fd7158685561
Mackenzie <ma.luzarow@gmail.com>
2018-07-02 19:52:55 -0500

    Moved home css to seperate file.

--- app/ViewItems/PageViews/HomeView.php
+++ app/ViewItems/PageViews/HomeView.php
@@ -9,78 +9,7 @@ class HomeView extends ViewAbstract {
 	 */
 	protected function constructCSS () {
 		$output =
-		'<style>
-			.home_header {
-				margin: 0.5em 0;
-				padding-bottom: 0.5em;
-				color: #c19356;
-				text-align: center;
-				font-family: Consolas;
-				font-size: 2em;
-			}
-			
-			.home_header::after {
-				position: relative;
-				top: 16px;
-				margin: 0 auto;
-				width: 500px;
-				display: block;
-				border-bottom: 1px solid #d68100;
-				content: "";
-			}
-			
-			.statbox_grid_display {
-				padding: 50px;
-				display: grid;
-				grid-column-gap: 50px;
-				grid-row-gap: 50px;
-				grid-template-columns: auto auto auto;
-				box-sizing: border-box;
-			}
-			
-			.statbox_grid_display .statbox_wrap {
-				padding: 10px;
-				background-color: #aaa78c;
-				box-shadow: 0 0 9px 4px rgba(0, 0, 0,0.2);
-				box-sizing: border-box;
-				font-family: Consolas;
-			}
-			
-			.statbox_grid_display .statbox_wrap .title {
-				padding-bottom: 10px;
-				display: block;
-				text-align: center;
-				font-size: 2em;
-			}
-			
-			.statbox_grid_display .statbox_wrap .statbox_inner_wrap span {
-				display: block;
-				text-align: center;
-				font-size: 3em;
-			}
-			
-			@media all and (max-width: 768px) {
-				.statbox_grid_display {
-					grid-template-columns: auto auto;
-				}
-			}
-			
-			@media all and (max-width: 500px) {
-				.statbox_grid_display {
-					padding: 25px;
-					grid-row-gap: 25px;
-					grid-template-columns: auto;
-				}
-				
-				.statbox_grid_display .statbox_wrap .title {
-					font-size: 1.6em;
-				}
-				
-				.statbox_grid_display .statbox_wrap .statbox_inner_wrap span {
-					font-size: 2.3em;
-				}
-			}
-		</style>';
+		'<link rel="stylesheet" type="text/css" href="/ViewItems/CSS/Home.css">';
 		
 		return ($output);
 	}

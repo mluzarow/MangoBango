5d9ab49bc1c9807922deb55834dae1420c6432b1
Mackenzie <ma.luzarow@gmail.com>
2018-07-02 19:22:02 -0500

    Updated Jikan connector to use connector parent.

--- app/Connectors/Jikan.php
+++ app/Connectors/Jikan.php
@@ -1,13 +1,15 @@
 <?php
 namespace Connectors;
 
+use \Connectors\Connector;
+
 /**
  * Connector for the Jikan Unofficial My Anime List API. Used to draw metadata
  * for manga.
  * 
  * @link https://jikan.moe/ Jikan API Documentation
  */
-class Jikan {
+class Jikan extends Connector {
 	/**
 	 * @var string Jikan API base URL
 	 */
@@ -24,37 +26,17 @@ class Jikan {
 		$query = trim ($query);
 		
 		if (empty ($query)) {
-			throw new \InvalidArgumentException ('Argument (Query) of Jikan::searchByQueryString cannot be empty.');
+			throw new \InvalidArgumentException ('Argument (Query) of searchByQueryString() cannot be empty.');
 		} else if (count ($query) < 2) {
-			throw new \InvalidArgumentException ('Argument (Query) of Jikan::searchByQueryString must be a minimum of 3 letters.');
+			throw new \InvalidArgumentException ('Argument (Query) of searchByQueryString() must be a minimum of 3 letters.');
 		}
 		
 		$query = urldecode ($query);
 		
 		$url = $this->jikan_base_url.'search/manga/'.$query.'/1';
 		
-		$result = json_decode ($this->request ($url), true);
+		$result = json_decode ($this->requestGET ($url), true);
 		
 		return ($result);
-	}
-	
-	/**
-	 * Performs a cURL request with the constructed URL.
-	 * 
-	 * @param string $url constructed URL to be used with Jikan
-	 * 
-	 * @return string JSON response from the Jikan servers
-	 */
-	private function request ($url) {
-		$curl = curl_init ();
-		
-		curl_setopt ($curl, CURLOPT_URL, $url);
-		curl_setopt ($curl, CURLOPT_RETURNTRANSFER, true);
-		
-		$response = curl_exec ($curl);
-		
-		curl_close ($curl);
-		
-		return ($response);
 	}
 }

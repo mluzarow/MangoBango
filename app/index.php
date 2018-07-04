b2304085292199030ae7bbd0ccf8454eb630ea97
Mackenzie <ma.luzarow@gmail.com>
2018-07-03 20:47:11 -0500

    Moved frame html into meta page.

--- app/index.php
+++ app/index.php
@@ -20,78 +20,6 @@ spl_autoload_register(function ($className) {
 
 \Core\Database::initialize ();
 
-echo
-'<!DOCTYPE html>
-<html>
-	<head>
-		<script type="text/javascript" src="/External/Javascript/jquery-3.3.1.js"></script>
-		<script type="text/javascript" src="/ViewItems/JS/dropdown.js"></script>
-		<link rel="stylesheet" type="text/css" href="/ViewItems/CSS/UIFrame.css">
-	</head>
-	<body>
-		<div class="topbar">
-			<div class="logo">
-				<a href="\">MangoBango</a>
-			</div>
-			<div class="icons_wrap">
-				<div class="button btn_burger dropdown_menu_button">
-					<img src="\resources\icons\burger.svg" />
-				</div>
-				<div class="burger_dropdown dropdown_menu">
-					<div class="menu_item">
-						<a href="/import">
-							<img src="\resources\icons\upload.svg" />
-							<span>Importer</span>
-						</a>
-					</div>
-					<div class="menu_item">
-						<a href="/displaylibrary">
-							<img src="\resources\icons\bookshelf.svg" />
-							<span>Library</span>
-						</a>
-					</div>
-					<div class="menu_item">
-						<a href="/config">
-							<img src="\resources\icons\gears.svg" />
-							<span>Settings</span>
-						</a>
-					</div>
-					<div class="menu_item">
-						<a href="/db/dashboard">
-							<img src="\resources\icons\database.svg" />
-							<span>Database</span>
-						</a>
-					</div>
-				</div>
-				<div class="button btn_import">
-					<a href="/import">
-						<img src="\resources\icons\upload.svg" />
-					</a>
-				</div>
-				<div class="button btn_library">
-					<a href="/displaylibrary">
-						<img src="\resources\icons\bookshelf.svg" />
-					</a>
-				</div>
-				<div class="flyout library">
-					<div class="search_wrap">
-						<input class="search_box" type="text" autocomplete="off" />
-					</div>
-				</div>
-				<div class="button btn_config">
-					<a href="/config">
-						<img src="\resources\icons\gears.svg" />
-					</a>
-				</div>
-				<div class="button btn_db">
-					<a href="/db/dashboard">
-						<img src="\resources\icons\database.svg" />
-					</a>
-				</div>
-			</div>
-		</div>
-		<div class="display_container">';
-
 // Parse the URL here
 $url_split = explode ('?', $_SERVER['REQUEST_URI']);
 
@@ -145,7 +73,4 @@ if (!empty($current_segs)) {
 	new \Controllers\Home ();
 }
 
-echo 
-		'</div>
-	</body>
-</html>';
+echo \Core\MetaPage::render ();

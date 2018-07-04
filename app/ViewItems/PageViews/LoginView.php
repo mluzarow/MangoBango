e9ef42366dc3926a35ec676dcad05e41b4238bf9
Mackenzie <ma.luzarow@gmail.com>
2018-07-04 15:33:19 -0500

    Created login view file.

---
+++ app/ViewItems/PageViews/LoginView.php
@@ -0,0 +1,11 @@
+<?php
+namespace ViewItems\PageViews;
+
+use \ViewItems\ViewAbstract;
+
+/**
+ * Page view rendering output for the server login page.
+ */
+class LoginView extends ViewAbstract {
+	
+}

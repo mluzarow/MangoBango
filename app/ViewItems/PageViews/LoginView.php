dd0cef97a6daf187177c4a551af3c6339a6dec54
Mackenzie <ma.luzarow@gmail.com>
2018-07-04 15:39:09 -0500

    Added required abstract methods.

--- app/ViewItems/PageViews/LoginView.php
+++ app/ViewItems/PageViews/LoginView.php
@@ -7,5 +7,24 @@ use \ViewItems\ViewAbstract;
  * Page view rendering output for the server login page.
  */
 class LoginView extends ViewAbstract {
+	/**
+	 * Constructs the CSS using the available properties.
+	 */
+	protected function constructCSS () {
+		return ('');
+	}
 	
+	/**
+	 * Constructs the HTML using the available properties.
+	 */
+	protected function constructHTML () {
+		return ('');
+	}
+	
+	/**
+	 * Constructs the javascript using the available properties.
+	 */
+	protected function constructJavascript () {
+		return ('');
+	}
 }

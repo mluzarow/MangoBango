99acbfb6004f946cc26d94d09ad834089aeee994
Mackenzie <ma.luzarow@gmail.com>
2018-07-04 20:04:55 -0500

    Added click even to js.

--- app/ViewItems/JS/Login.js
+++ app/ViewItems/JS/Login.js
@@ -1,3 +1,10 @@
 $(window).ready (function () {
-	
+	$("#login_btn").click (function () {
+		let login = {
+			'username' : $("#username_field").val (),
+			'password' : $("#password_field").val ()
+		};
+		
+		
+	});
 });

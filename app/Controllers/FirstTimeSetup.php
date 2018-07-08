1ffc4943941c7df12e03f85687baf22fb323f4e4
Mackenzie <ma.luzarow@gmail.com>
2018-07-07 23:30:16 -0500

    Added page controller for first time setup.

---
+++ app/Controllers/FirstTimeSetup.php
@@ -0,0 +1,14 @@
+<?php
+namespace Controllers;
+
+/**
+ * Page controller for setting up the server for the first time.
+ */
+class FirstTimeSetup {
+	/**
+	 * Constructor for page controller FirstTimeSetup.
+	 */
+	public function __construct () {
+		
+	}
+}

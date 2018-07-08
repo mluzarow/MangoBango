368edb2d738a4dad74616c9e99ec4dedf2ac2acd
Mackenzie <ma.luzarow@gmail.com>
2018-07-08 10:25:29 -0500

    Added first time setup page view.

---
+++ app/ViewItems/PageViews/FirstTimeSetupView.php
@@ -0,0 +1,39 @@
+<?php
+namespace ViewItems\PageViews;
+
+use \ViewItems\ViewAbstract;
+
+/**
+ * Page view for the first time server setup script.
+ */
+class FirstTimeSetupView extends ViewAbstract {
+	/**
+	 * Constructs the CSS using the available properties.
+	 */
+	protected function constructCSS () {
+		$output =
+		'';
+		
+		return ($output);
+	}
+	
+	/**
+	 * Constructs the HTML using the available properties.
+	 */
+	protected function constructHTML () {
+		$output =
+		'';
+		
+		return ($output);
+	}
+	
+	/**
+	 * Constructs the javascript using the available properties.
+	 */
+	protected function constructJavascript () {
+		$output =
+		'';
+		
+		return ($output);
+	}
+}

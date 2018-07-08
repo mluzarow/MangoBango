02ecb8a869b217ca7376c857f8b0690582904722
Mackenzie <ma.luzarow@gmail.com>
2018-07-08 12:35:43 -0500

    Added ajax call to start table creation.

--- app/ViewItems/JS/FirstTimeSetup.js
+++ app/ViewItems/JS/FirstTimeSetup.js
@@ -1,3 +1,17 @@
 $(window).ready (function () {
+	$(".setup_start_btn").click (function () {
+		$.ajax ({
+			url: "ajax/Controllers/FirstTimeSetup/ajaxCreateDatabases",
+			method: "POST",
+			data: {
+				ajax: 1
+			}
+		}).done (function (response) {
+			generateMessageList (repsonse);
+		});
+	});
 	
+	function generateMessageList (msgList) {
+		alert (msgList);
+	}
 });

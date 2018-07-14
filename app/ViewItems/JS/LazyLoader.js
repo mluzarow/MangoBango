fafd655cf591af90e0e36809b28aa11e4dd6ccc9
Mackenzie <ma.luzarow@gmail.com>
2018-07-14 15:01:47 -0500

    Added lazy loader script file.

---
+++ app/ViewItems/JS/LazyLoader.js
@@ -0,0 +1,8 @@
+/**
+ * Lazy image loader class. Loads images by looking for placeholder divs (.placeholder),
+ * replacing the HTML content with the image requested by the data-origin attribute
+ * on said placeholder div.
+ */
+class LazyLoader {
+	
+}

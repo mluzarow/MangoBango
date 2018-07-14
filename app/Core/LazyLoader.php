d91251de2f88795f9ab6e0ebccd5abf5e7738a3d
Mackenzie <ma.luzarow@gmail.com>
2018-07-14 16:18:02 -0500

    Corrected issues for lazy loader controller ajax methods.

--- app/Core/LazyLoader.php
+++ app/Core/LazyLoader.php
@@ -8,12 +8,12 @@ class LazyLoader {
 	/**
 	 * Requests an image via AJAX with the given image path.
 	 * 
-	 * @param string $image_path path to the requested image file
-	 * 
 	 * @return string base 64 encoded requested image file or empty string if no
 	 *                file could be found or loaded
 	 */
-	public function ajaxRequestImage (string $image_path) : string {
+	public function ajaxRequestImage () : string {
+		$image_path = $_POST['filepath'];
+		
 		$f = fopen ($image_path, 'r');
 		
 		if ($f === false) {
@@ -36,12 +36,10 @@ class LazyLoader {
 	/**
 	 * Requests a list of images via AJAX with a given list of image paths.
 	 * 
-	 * @param array $image_paths list of paths to the requested image files
-	 * 
 	 * @return array list of base 64 encoded requested image files
 	 */
-	public function ajaxRequestImages (array $image_paths) : string {
-		$image_datas = [];
+	public function ajaxRequestImages () : string {
+		$image_datas = json_decode ($_POST['filepaths'], true);
 		
 		foreach ($image_paths as $image_path) {
 			$image_datas[] = $this->ajaxRequestImage ($image_path);

9736f58c98e2f9453ab9c6cc38739c9d94f46318
Mackenzie <ma.luzarow@gmail.com>
2018-07-15 00:21:57 -0500

    Added manga info fetch to metadata manager.

--- app/Core/MetadataManager.php
+++ app/Core/MetadataManager.php
@@ -15,8 +15,49 @@ class MetadataManager {
 	private $jikan;
 	private $manga_covers;
 	
-	public function __contruct () {
+	/**
+	 * Constructor for controller MetadataManager.
+	 */
+	public function __construct () {
 		$this->jikan = new Jikan ();
 		$this->manga_covers = new MangaCovers ();
+	}
+	
+	/**
+	 * Fetches manga info by the given query string.
+	 * 
+	 * @param string $manga_name name of the manga to look for
+	 * 
+	 * @return array fetched manga information
+	 * 
+	 * @throws TypeError on non-string parameter & non-array return
+	 */
+	public function fetchMangaInfo (string $manga_name) : array {
+		$manga_result = $this->jikan->searchByQueryString ($manga_name);
+		
+		if (empty($manga_result['result'])) {
+			return ([]);
+		}
+		
+		$manga_info = $this->jikan->searchByMangaID (
+			(int) $manga_result['result'][0]['mal_id']
+		);
+		
+		if (empty($manga_info)) {
+			return ([]);
+		}
+		
+		$final_info = [
+			'title' => $manga_info['title'],
+			'title_original' => $manga_info['title_japanese'],
+			'summary' => $manga_info['synopsis'],
+			'genres' => []
+		];
+		
+		foreach ($manga_info['genre'] as $genre) {
+			$final_info['genres'][] = $genre['name'];
+		}
+		
+		return ($final_info);
 	}
 }

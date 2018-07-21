b93be6c720f8c40200429e796020f8ea09db751b
mluzarow <ma.luzarow@gmail.com>
2018-07-21 09:11:29 -0500

    Added abstract methods.

--- app/ViewItems/Partial/StatboxTextView.php
+++ app/ViewItems/Partial/StatboxTextView.php
@@ -7,5 +7,26 @@ use \ViewItems\ViewAbstract;
  * Partial view class for statistics box displaying text.
  */
 class StatboxTextView implements ViewAbstract {
+	/**
+	 * Constructs the CSS using the available properties.
+	 */
+	protected function constructCSS () {
+		return ('');
+	}
 	
+	/**
+	 * Constructs the HTML using the available properties.
+	 */
+	protected function constructHTML () {
+		$output = '';
+		
+		return ($output);
+	}
+	
+	/**
+	 * Constructs the javascript using the available properties.
+	 */
+	protected function constructJavascript () {
+		return ('');
+	}
 }

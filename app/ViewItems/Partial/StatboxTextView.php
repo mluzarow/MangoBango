7ba2ca0c06ea3480d070b639cc78e417498cb4b8
mluzarow <ma.luzarow@gmail.com>
2018-07-20 23:20:54 -0500

    Added statbox text partial view file.

---
+++ app/ViewItems/Partial/StatboxTextView.php
@@ -0,0 +1,11 @@
+<?php
+namespace ViewItems\Partial;
+
+use \ViewItems\ViewAbstract;
+
+/**
+ * Partial view class for statistics box displaying text.
+ */
+class StatboxTextView implements ViewAbstract {
+	
+}

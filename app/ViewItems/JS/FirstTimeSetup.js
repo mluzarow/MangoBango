ba09f6c3ee23375f5dc81d850942a079d001e313
mluzarow <ma.luzarow@gmail.com>
2018-09-29 15:45:35 -0500

    Updated js to make a single ajax call.

--- app/ViewItems/JS/FirstTimeSetup.js
+++ app/ViewItems/JS/FirstTimeSetup.js
@@ -1,30 +1,37 @@
 $(window).ready (function () {
 	$(".setup_start_btn").click (function () {
-		$.ajax ({
-			url: "ajax/Controllers/FirstTimeSetup/ajaxCreateDatabases",
-			method: "POST",
-			data: {
-				ajax: 1
-			}
-		}).done (function (response) {
-			$(".section_two").removeClass ("section_two");
-		});
+		$(".section_two").removeClass ("section_two");
 	});
 	
 	$(".add_user_btn").click (function () {
+		let username = $("#username").val ();
+		let password = $("#password").val ();
+		
+		// Validate username and password
+		if (username.length < 1) {
+			alert ("Username can not be blank.");
+			return;
+		}
+		
+		if (password.length < 1) {
+			alert ("Password can not be blank.");
+			return;
+		}
+		
 		$.ajax ({
-			url: "ajax/Core/SessionManager/ajaxCreateUser",
+			url: "ajax/Controllers/FirstTimeSetup/ajaxRunSetup",
 			method: "POST",
 			data: {
-				username: $("#username").val (),
-				password: $("#password").val ()
+				username : username,
+				password : password
+			},
+			success : function () {
+				$(".section_three").removeClass ("section_three");
+				
+				setTimeout (function () {
+					window.location = "/login";
+				}, 10000);
 			}
-		}).done (function (response) {
-			$(".section_three").removeClass ("section_three");
-			
-			setTimeout (function () {
-				window.location = "/login";
-			}, 10000);
 		});
 	});
 });

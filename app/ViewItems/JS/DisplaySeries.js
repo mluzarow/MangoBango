28e56596263b0d77882e3e759cdc954379a0fe45
mluzarow <ma.luzarow@gmail.com>
2018-10-07 18:04:45 -0500

    Moved common lazy loader code to events file.

--- app/ViewItems/JS/DisplaySeries.js
+++ app/ViewItems/JS/DisplaySeries.js
@@ -16,26 +16,5 @@ $(window).ready (function () {
 	}
 });
 
-$(window).on ("load", function () {
-	var lazyLoader = new LazyLoader ();
-	
-	// Request first batch of images on load once
-	var scrollTimeout = true;
-	
-	lazyLoader.replacePlaceholders (
-		lazyLoader.findPlaceholdersViewport ()
-	);
-	
-	setTimeout (() => scrollTimeout = false, 1000);
-	
-	$(window).scroll (function () {
-		if (scrollTimeout === false)
-			scrollTimeout = true;
-		
-		lazyLoader.replacePlaceholders (
-			lazyLoader.findPlaceholdersViewport ()
-		);
-		
-		setTimeout (() => scrollTimeout = false, 1000);
-	});
-});
+// Start lazy loader
+lazyLoadByScroll ();
--- app/ViewItems/JS/ReaderStrip.js
+++ app/ViewItems/JS/ReaderStrip.js
@@ -1,26 +1,5 @@
 /*
 	Page JS for view ReaderStripView.
  */
-$(window).on ("load", function () {
-	var lazyLoader = new LazyLoader ();
-	
-	// Request first batch of images on load once
-	var scrollTimeout = true;
-	
-	lazyLoader.replacePlaceholders (
-		lazyLoader.findPlaceholdersViewport ()
-	);
-	
-	setTimeout (() => scrollTimeout = false, 1000);
-	
-	$(window).scroll (function () {
-		if (scrollTimeout === false)
-			scrollTimeout = true;
-		
-		lazyLoader.replacePlaceholders (
-			lazyLoader.findPlaceholdersViewport ()
-		);
-		
-		setTimeout (() => scrollTimeout = false, 1000);
-	});
-});
+// Start lazy loader
+lazyLoadByScroll ();

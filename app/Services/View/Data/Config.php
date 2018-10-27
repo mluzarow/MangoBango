551ae7040c557b9f3ca0fb3820b6ff87e5b661d7
mluzarow <mluzarow@users.noreply.github.com>
2018-10-26 20:21:23 -0500

    Added Config view data obj file.

---
+++ app/Services/View/Data/Config.php
@@ -0,0 +1,14 @@
+<?php
+namespace Services\View\Data;
+
+/**
+ * View data object for the Config view.
+ */
+class Config implements IViewData {
+	/**
+	 * Constructor for data object.
+	 */
+	public function __construct () {
+		
+	}
+}

fb9479a52e30162773ba8ac2f4c0f2a1f1bc53e9
mluzarow <mluzarow@users.noreply.github.com>
2018-10-27 08:47:05 -0500

    Added public getters.

--- app/Services/View/Data/DisplaySeries.php
+++ app/Services/View/Data/DisplaySeries.php
@@ -15,4 +15,67 @@ class DisplaySeries implements IViewData {
 	public function getViewName () : string {
 		return 'DisplaySeries';
 	}
+	
+	/**
+	 * Gets chapter anchor data list.
+	 * 
+	 * @return array chapter data list
+	 * 
+	 * @throws TypeError on non-array return
+	 */
+	public function getChapters () : array {
+		return $this->chapters;
+	}
+	
+	/**
+	 * Gets list of series genres.
+	 * 
+	 * @return array list of series genres
+	 * 
+	 * @throws TypeError on non-array return
+	 */
+	public function getGenres () : array {
+		return $this->genres;
+	}
+	
+	/**
+	 * Gets series summary.
+	 * 
+	 * @return string series summary
+	 * 
+	 * @throws TypeError on non-string return
+	 */
+	public function getSummary () : string {
+		if (empty ($this->summary)) {
+			return '<i>No summary available.</i>';
+		} else {
+			return $this->summary;
+		}
+	}
+	
+	/**
+	 * Gets manga title.
+	 * 
+	 * @return string manga title
+	 * 
+	 * @throws TypeError on non-string return
+	 */
+	public function getTitle () : string {
+		if (empty ($this->title)) {
+			return '<i>No Title</i>';
+		} else {
+			return $this->title;
+		}
+	}
+	
+	/**
+	 * Gets display data for each volume.
+	 * 
+	 * @return array display data for each volume
+	 * 
+	 * @throws TypeError on non-array return
+	 */
+	public function getVolumes () : array {
+		return $this->volumes;
+	}
 }

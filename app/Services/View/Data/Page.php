8e0a6f4f574ae7e85fbbc33c72c4e641e2c2f48c
mluzarow <ma.luzarow@gmail.com>
2018-10-28 12:24:46 -0500

    Added rest of page view data obj.

--- app/Services/View/Data/Page.php
+++ app/Services/View/Data/Page.php
@@ -5,5 +5,78 @@ namespace Services\View\Data;
  * View data object for the Page view.
  */
 class Page implements IViewData {
+	/**
+	 * @var string   $title         page title
+	 * @var ViewItem $view_content  view content
+	 */
+	private $title;
+	private $view_content;
 	
+	/**
+	 * Constructor for data object.
+	 * 
+	 * @param string   $title        page title
+	 * @param ViewItem $view_content view content
+	 * 
+	 * @throws TypeError on invalid parameter type
+	 */
+	public function __construct (string $title, ViewItem $view_content) {
+		$this->setTitle ($title);
+		$this->setViewContent ($view_content);
+	}
+	
+	/**
+	 * Gets page title.
+	 * 
+	 * @return string page title
+	 * 
+	 * @throws TypeError on non-string return
+	 */
+	public function getTitle () : string {
+		return $this->title;
+	}
+	
+	/**
+	 * Gets view content.
+	 * 
+	 * @return ViewItem view content
+	 * 
+	 * @throws TypeError on non-ViewItem return
+	 */
+	public function getViewContent () : ViewItem {
+		return $this->view_content;
+	}
+	
+	/**
+	 * Gets the view name to which the data is tied (the controller's name).
+	 * 
+	 * @return string page controller name
+	 * 
+	 * @throws TypeError on non-string return
+	 */
+	public function getViewName () : string {
+		return 'Page';
+	}
+	
+	/**
+	 * Sets page title.
+	 * 
+	 * @param string $title page title
+	 * 
+	 * @throws TypeError on non-string parameter
+	 */
+	private function setTitle (string $title) {
+		$this->title = $title;
+	}
+	
+	/**
+	 * Sets view content.
+	 * 
+	 * @param string $view_content view content
+	 * 
+	 * @throws TypeError on non-ViewItem parameter
+	 */
+	private function setViewContent (ViewItem $view_content) {
+		$this->view_content = $view_content;
+	}
 }

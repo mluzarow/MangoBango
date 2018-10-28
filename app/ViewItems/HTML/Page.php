e84c450ece1b2d26c82d96a0014ba386ab521396
mluzarow <ma.luzarow@gmail.com>
2018-10-28 12:47:05 -0500

    Fixed typo on view obj method.

--- app/ViewItems/HTML/Page.php
+++ app/ViewItems/HTML/Page.php
@@ -2,11 +2,11 @@
 <html>
 	<head>
 		<title><?php echo $view->getTitle (); ?></title>
-		<?php echo $view->getViewContents ()->getCSS (); ?>
+		<?php echo $view->getViewContent ()->getCSS (); ?>
 		<script type="text/javascript" src="/External/Javascript/jquery-3.3.1.js"></script>
-		<?php echo $view->getViewContents ()->getJS (); ?>
+		<?php echo $view->getViewContent ()->getJS (); ?>
 	</head>
 	<body>
-		<?php echo $view->getViewContents ()->getHTML (); ?>
+		<?php echo $view->getViewContent ()->getHTML (); ?>
 	</body>
 </html>

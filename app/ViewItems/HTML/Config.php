a703ed0fea7c67d43fd283797c02a13ff209d390
mluzarow <ma.luzarow@gmail.com>
2018-11-23 12:11:40 -0600

    Split reader settings from library settings.

--- app/ViewItems/HTML/Config.php
+++ app/ViewItems/HTML/Config.php
@@ -5,6 +5,24 @@
 			<div class="section_header">Library Settings</div>
 			<div class="config_wrap">
 				<div class="config_item">
+					<label>Manga directory</label>
+					<input id="manga_directory" value="<?php echo $view->getMangaDirectory (); ?>" type="text" autocomplete="off" />
+				</div>
+				<div class="config_item">
+					<label>Directory structure</label>
+					<input id="directory_structure" value="<?php echo $view->getDirectoryStructure  (); ?>" type="text" autocomplete="off" />
+				</div>
+				<div class="config_item">
+					<div id="rescan_library_btn">
+						Rescan Manga Library
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="config_section">
+			<div class="section_header">Reader Settings</div>
+			<div class="config_wrap">
+				<div class="config_item">
 					<label>Reader Display Style</label>
 					<select id="reader_display_style" autocomplete="off">
 						<option value="1" <?php echo $view->getReaderDisplayStyle () === 1 ? 'selected' : '' ?>>
@@ -16,14 +34,6 @@
 					</select>
 				</div>
 				<div class="config_item">
-					<label>Manga directory</label>
-					<input id="manga_directory" value="<?php echo $view->getMangaDirectory (); ?>" type="text" autocomplete="off" />
-				</div>
-				<div class="config_item">
-					<label>Directory structure</label>
-					<input id="directory_structure" value="<?php echo $view->getDirectoryStructure  (); ?>" type="text" autocomplete="off" />
-				</div>
-				<div class="config_item">
 					<label>Library Display Style</label>
 					<select id="library_view_type" autocomplete="off">
 						<option value="1" <?php echo $view->getLibraryViewType () === 1 ? 'selected' : ''; ?>>
@@ -33,11 +43,6 @@
 							Display as bookcase
 						</option>
 					</select>
-				</div>
-				<div class="config_item">
-					<div id="rescan_library_btn">
-						Rescan Manga Library
-					</div>
 				</div>
 			</div>
 		</div>

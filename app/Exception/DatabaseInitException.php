d5ca5bd686c0478613fc9b18c1bc7fd6a0295ea2
mluzarow <ma.luzarow@gmail.com>
2019-09-01 14:20:58 -0500

    Removed redundant calls to parent constructors.

--- app/Exception/DatabaseInitException.php
+++ app/Exception/DatabaseInitException.php
@@ -4,15 +4,4 @@ namespace Exception;
 /**
  * Exception regarding database initialization failures.
  */
-class DatabaseInitException extends \Exception {
-	/**
-	 * Construction for exception.
-	 *
-	 * @param string    $message  message to be printed
-	 * @param int       $code     exception code
-	 * @param Exception $previous previous exception
-	 */
-	public function __construct($message, $code = 0, Exception $previous = null) {
-		parent::__construct($message, $code, $previous);
-	}
-}
+class DatabaseInitException extends \Exception {}
--- app/Exception/DatabaseQueryException.php
+++ app/Exception/DatabaseQueryException.php
@@ -4,15 +4,4 @@ namespace Exception;
 /**
  * Exception regarding database query failures.
  */
-class DatabaseQueryException extends \Exception {
-	/**
-	 * Construction for exception.
-	 *
-	 * @param string    $message  message to be printed
-	 * @param int       $code     exception code
-	 * @param Exception $previous previous exception
-	 */
-	public function __construct($message, $code = 0, Exception $previous = null) {
-		parent::__construct($message, $code, $previous);
-	}
-}
+class DatabaseQueryException extends \Exception {}
